5668e7b2ddde1093248c8cdd02e538bd1a839ad0
andersonlouzada <anderson.s.louzada@gmail.com>
2023-07-12 17:12:25 -0300

    corrige query do requisito 6

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -1,7 +1,7 @@
 SELECT 
-	MIN(price) AS faturamento_minimo,
-  MAX(price) AS faturamento_maximo,
-  ROUND(AVG(price), 2) AS faturamento_medio,
-  ROUND(SUM(price), 2) AS faturamento_total
+	FORMAT(MIN(p.price), 2) AS faturamento_minimo,
+  FORMAT(MAX(p.price), 2) AS faturamento_maximo,
+  FORMAT(AVG(p.price), 2) AS faturamento_medio,
+  FORMAT(SUM(p.price), 2) AS faturamento_total
 FROM plans AS p
 INNER JOIN users as u ON u.plan_id = p.plan_id;
